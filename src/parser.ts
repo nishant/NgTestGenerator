e22fbf69d339955e41d1fe0c2e4c6e1fda99dd94
Nishant Arora <nish54@gmail.com>
2021-08-30 22:09:49 -0500

    added functionality to read file content by path

--- src/parser.ts
+++ src/parser.ts
@@ -1,9 +1,15 @@
-import { getHtmlFiles, getWebAppDir } from './preload-helpers';
+import { getHtmlFiles, getWebAppDir, readFile } from './preload-helpers';
 
 console.log('Running Parser...\n');
 
-const webappRootDir = getWebAppDir();
-const htmlFiles = getHtmlFiles(webappRootDir);
+const webappRootDir: string = getWebAppDir();
+const htmlFiles: Array<string> = getHtmlFiles(webappRootDir);
+const fileMap: Map<string, string> = new Map();
+
+htmlFiles.forEach((file) => {
+  fileMap.set(file, readFile(file));
+});
 
 console.log(`Webapp Root Directory: ${webappRootDir}\n`);
 console.log(`HTML Files Found:\n* ${htmlFiles.join('\n')}`);
+console.log(fileMap);
--- src/preload-helpers.ts
+++ src/preload-helpers.ts
@@ -1,9 +1,34 @@
-import { glob } from 'glob';
+import * as fs from 'fs';
 
 export const getWebAppDir = () => {
   const args = process.argv.slice(2);
   return args[0];
 };
 
-export const getHtmlFiles = (webappRootDir: string) =>
-  glob.sync(`${webappRootDir}/src/**/*.component.html`);
+export const getHtmlFiles = (
+  webappRootDir: string,
+  files_: Array<string> = [],
+) => {
+  const files = fs.readdirSync(webappRootDir);
+
+  files.forEach((filename) => {
+    const path = `${webappRootDir}/${filename}`;
+
+    if (fs.statSync(path).isDirectory()) {
+      getHtmlFiles(path, files_);
+    } else if (path.endsWith('.component.html')) {
+      files_.push(path);
+    }
+  });
+
+  return files_;
+};
+
+export const readFile = (path: string) => {
+  try {
+    return fs.readFileSync(path, 'utf8');
+  } catch (err) {
+    console.error(err);
+  }
+  return '';
+};
